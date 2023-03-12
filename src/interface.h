3e414e884fb9e110e953ad97ea9e9ad3fbbed51e
Anastasia Andreeva <95581755+woaml@users.noreply.github.com>
2023-03-12 09:50:42 +0000

    add comments to interface.h

--- src/interface.h
+++ src/interface.h
@@ -3,6 +3,14 @@
 
 #include "stddef.h"
 
+// This function is used in main.c client file. Implementation is in
+// server.c server file
+
+// reverseWords
+// Function reverses the whole string and then reverses each word in the string
+// Arguments:
+//    str - string in which to reverse all the words
+
 void reverseWords(char* str);
 
 #endif
